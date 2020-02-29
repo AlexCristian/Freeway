a8ca57fb1ee5298dda4c9cecb947bb2f591c106e
Harun Gunaydin <harun@drexel.edu>
2020-02-29 18:06:33 -0500

    3 new endpoints (#24)
    * 3 new endpoints
    
    * Changes-1

---
+++ api/endpoints/tasks.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import ValidationError
+from django.http import HttpResponse
+from api.models import Task, User
+from api.common import *
+
+# Task-related endpoints reside here.
+
+# URI: /api/task
+# Expect: description
+def task(request):
+    expected_fields = ["description"]
+
+    try:
+        json_req = getSafeJsonFromBody(expected_fields, request.body.decode("utf-8"))
+    except UnexpectedContentException:
+        return httpBadRequest()
+
+    user = None
+    try:
+        user = User.objects.get(email=request.session["uemail"])
+    except (ObjectDoesNotExist, KeyError):
+        return HttpResponse("Unauthorized", status=401)
+
+    try:
+        Task.objects.create(
+            description=json_req["description"],
+            pinid=user.id,
+            volunteerid=user.id,  # May not be the best choice but can't leave it null
+            state=Task.POSTED,
+        )
+    except ValidationError:
+        return HttpResponse(
+            "Invalid task data",
+            status=400
+        )
+    else:
+        return HttpResponse(
+            "New task added to DB",
+            status=200
+        )
