afcb21e1a29eaef308881555d7a08540dee6bc51
Alexandru Cristian <salut@acristian.eu>
2020-03-11 22:35:07 -0400

    Add Azure URL to whitelist, attempt CORS fix.

--- Freeway/settings.py
+++ Freeway/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 's!a6gqsjvhb96_g&$sy!y!7=(e%6$*l8)70ouk3rc1^s!wkimj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'freeway.eastus.cloudapp.azure.com' ]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'Freeway.urls'
 
