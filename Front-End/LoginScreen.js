962942ca291bdb3e57e2ea8655171930db0b8d2b
crazyasianpersuasion <35272972+crazyasianpersuasion@users.noreply.github.com>
2020-05-11 18:26:13 -0400

    Updated Login Screen for iOS
    Updated Login Screen for iOS

--- Front-End/LoginScreen.js
+++ Front-End/LoginScreen.js
@@ -1,28 +1,93 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, ScrollView, TouchableOpacity, ActivityIndicator, FlatList} from 'react-native';
+import {StyleSheet, Text, View, Button, ScrollView, TouchableOpacity, ActivityIndicator, FlatList, RecyclerViewBackedScrollView} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {Input} from 'react-native-elements';
 import {Image} from 'react-native' ; 
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator, createAppContainer} from '@react-navigation/stack';
-
+import * as Google from 'expo-google-app-auth';
 
 export default class LoginScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        isLoading: true,
-        dataSource: null,
-        loginAttempt: false,
+            loginAttempt: false,
+            signedIn: false,
+            name: "",
+            photoUrl: "",
+            email: "",
+            userInfo: null,
+            idToken: null
         }
     }
 
-    componentDidMount(){
+    enrollUser(){
+        makeLogin(this).catch(error => {
+            // User needs to sign up first
+            signUpUser(this).then(resp => {
+                makeLogin(this)
+            })
+        })
+        
+    }
+
+
+    getGoogleProfile = async () => {
+        try {
+            const result = await Google.logInAsync({
+                androidClientId: "742738163356-hf5tdgnfa5dec8e5ii42jr28lugkc6ak.apps.googleusercontent.com",
+                iosClientId: "742738163356-tpo7qt8cvqno2cs739k8uh7sp9c9oq58.apps.googleusercontent.com",
+                scopes: ["profile", "email"]
+            })
+    
+            if (result.type === "success") {
+                this.setState({
+                    name: result.user.name,
+                    photoUrl: result.user.photoUrl,
+                    email: result.user.email,
+                    idToken: result.idToken
+                })
+                
+                this.enrollUser()
+            } 
+            else {
+                console.log("cancelled")
+            }
+        }
+        catch (e) {
+            console.log("Get Google profile error", e)
+        }
+        
+    } 
+
+    
+    render() {
+        
+        return (
+            <View style={styles.container}>
+            {this.state.signedIn ? (
+                this.props.navigation.navigate('Profile')
+                
+            ) : (
+                <LoginPage getGoogleProfile={this.getGoogleProfile} />
+            )}
+            </View>
+        )
+    }
+}
+
+function makeLogin(context) {
+    return new Promise(function (resolve, reject) {
+        email = context.state.email
+        idToken = context.state.idToken
+        
+        console.log("Making Login");
+
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
-        var raw = JSON.stringify({"email":"a@gmail.com","oauthid":"123456"});
+        var raw = JSON.stringify({"email":email,"oauthid":idToken});
 
         var requestOptions = {
             method: 'POST',
@@ -31,92 +96,85 @@ export default class LoginScreen extends React.Component {
             body: raw,
             redirect: 'follow'
         };
-        return fetch("http://freeway.eastus.cloudapp.azure.com:8000/api/login", requestOptions)
-            .then(response => response.text())
+
+        fetch("http://freeway.eastus.cloudapp.azure.com:8000/api/login", requestOptions)
             .then(result => {
-                this.setState({
-                isLoading: false,
-                dataSource: result,
-                });
+                if (result.ok){
+                    context.setState({signedIn: true})
+                    console.log("Login success.")
+                    resolve(result.text)
+                } else {
+                    console.log("Login fail.")
+                    reject(result.status)
+                }
             })
-            .catch(error => console.log('error', error));
-        
-    }
-
-    //setTrue = () => {
-        //this.setState({loginAttempt: true})
-    //}
-  
-    render() {
-
-        if (this.state.LoginAttempt == true) {
-            this.props.navigation.navigate('Profile')
-        }
-            
-        
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.container}>
-                <ActivityIndicator />
-                </View>
-            )
-        }
-        else {
-            return (
-                <View style={styles.login}>
-            
-                    <Image source={require('./assets/Logo.png')} style={{width: 200, height: 200}} />
-            
-                    <Text style={{fontSize: 40}}>Freeway</Text>
-            
-                    <Text style={{fontSize: 20}}>Welcome to our application!</Text>
-                    
-                    <Text style={{flexWrap: 'wrap', textAlign: 'center'}}>Our mission is to provide a way for humans to help one another through a simple matching process</Text>
-                    <Text>{' '}{' '}</Text>
-
-                    <TouchableOpacity onPress={() => this.setState({LoginAttempt: true})} >
-                        <Text >
-                            Login
-                        </Text>
-                    </TouchableOpacity >
-            
-                    
-            
-                </View>
-            )
-        }
-    }
+            .catch(error => console.log('Login error', error));
+    })
 }
 
-const styles = StyleSheet.create({
+function signUpUser(context) {
+    return new Promise(function (resolve, reject) {
+    console.log("signup")
 
-    login:{
-      flexGrow:1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-  
-    },
-  
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    name = context.state.name
+    photoURL = context.state.photoUrl
+    email = context.state.email
+    idToken = context.state.idToken
+
+    var raw = JSON.stringify({"email":email,"oauthid":idToken,"name":name, "photourl":photoURL, "location":" ", "bio":" "});
+
+    console.log(raw)
+
+    var requestOptions = {
+        method: 'POST',
+        credentials: 'include',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    fetch("http://freeway.eastus.cloudapp.azure.com:8000/api/signup", requestOptions)
+        .then(result => {
+            if (result.ok){
+                console.log("Signup success.")
+                resolve(result.text)
+            } else {
+                console.log("Signup fail.")
+                reject(result.status)
+            }
+        })
+        .catch(error => console.log('Signup error', error));
+    })
+}
+
+    
+const LoginPage = props => {
+    return (
+        <View>
+            <Text style={styles.header}>Sign In With Google</Text>
+            <Button title="Sign in with Google" onPress={() => props.getGoogleProfile()} />
+        </View>
+    )
+}
+    
+const styles = StyleSheet.create({
     container: {
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-  
-    },
-  
-    scrollView: {
-      alignItems: 'stretch',
-  
-  
-    },
-  
-    backButton: {
-      alignSelf: "flex-end",
-      alignItems:'center',
-      backgroundColor: '#aaa',
-      margin: 20,
-      padding: 10,
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+        },
+        header: {
+        fontSize: 25
+        },
+        image: {
+        marginTop: 15,
+        width: 150,
+        height: 150,
+        borderColor: "rgba(0,0,0,0.2)",
+        borderWidth: 3,
+        borderRadius: 150
     }
-  
-  });  
+})
--- Front-End/ProfileScreen.js
+++ Front-End/ProfileScreen.js
@@ -12,7 +12,7 @@ export default class ProfileScreen extends React.Component {
       this.state = {
         isLoading: true,
         dataSource: null,
-        newEmail: '',
+        newBio: '',
         newLoc: '',
 
       }
@@ -35,7 +35,9 @@ export default class ProfileScreen extends React.Component {
             isLoading: false,
             dataSource: result,
           });
+          console.log(result)
         })
+
         .catch(error => console.log('error', error));
     }
     
@@ -67,11 +69,12 @@ export default class ProfileScreen extends React.Component {
           <Text style = {{paddingTop: '2%', paddingLeft: '5%', paddingRight: '5%', fontSize: 25}}>{name}</Text>
           
           <Text style = {{paddingTop: '2%', paddingLeft: '5%', paddingRight: '5%', paddingBottom: '5%', fontSize: 25}}>{bio}</Text> 
+          <TextInput style={{paddingLeft: '5%', paddingRight: '5%', height: 40, fontSize: 13}} placeholder={bio} onChangeText={(newBio) => this.setState({newBio})} value={this.state.newBio}/>
     
     
           <View style={{flexDirection: 'row', justifyContent: 'space-between', }}>
             <Text style = {{paddingLeft: '5%', fontSize: 13}}>Email</Text>
-            <TextInput style={{paddingLeft: '5%', paddingRight: '5%', height: 40, fontSize: 13}} placeholder={email} onChangeText={(newEmail) => this.setState({newEmail})} value={this.state.newEmail}/>
+            <Text style={{paddingLeft: '5%', paddingRight: '5%', height: 40, fontSize: 13}}> {email} </Text>
           </View>
 
           <View style={{flexDirection: 'row', justifyContent: 'space-between', }}>
@@ -81,13 +84,18 @@ export default class ProfileScreen extends React.Component {
           
 
           <View style={{flexDirection: 'row', justifyContent: 'space-between', flexGrow:1, margin: 20, padding: 10}}>
+            <TouchableOpacity onPress={() => {saveBio(this.state.newBio); saveLoc(this.state.newLoc)}} style={styles.backButton} >
+              <Text >
+                  Save Changes
+              </Text>
+            </TouchableOpacity >
             <TouchableOpacity onPress={() => this.props.navigation.navigate('NewTask')} style={styles.backButton} >
               <Text >
                   Create Task
               </Text>
             </TouchableOpacity >
 
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')} style={styles.backButton} >
+            <TouchableOpacity onPress={() => {logout(); this.props.navigation.navigate('Login')}} style={styles.backButton} >
               <Text >
                   Logout
               </Text>
@@ -105,6 +113,80 @@ export default class ProfileScreen extends React.Component {
       }
     }  
   }
+
+  function saveBio(bio){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+
+    var raw = JSON.stringify({"newbio":bio});
+
+    var requestOptions = {
+        method: 'POST',
+        credentials: 'include',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    return fetch("http://freeway.eastus.cloudapp.azure.com:8000/api/setbio", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            this.setState({
+            isLoading: false,
+            dataSource: result,
+            });
+        })
+        .catch(error => console.log('error', error));
+  }
+
+  function saveLoc(location) {
+    var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+  
+  
+      var raw = JSON.stringify({"newlocation ":location});
+  
+      var requestOptions = {
+          method: 'POST',
+          credentials: 'include',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+      };
+      return fetch("http://freeway.eastus.cloudapp.azure.com:8000/api/setlocation", requestOptions)
+          .then(response => response.text())
+          .then(result => {
+              this.setState({
+              isLoading: false,
+              dataSource: result,
+              });
+          })
+          .catch(error => console.log('error', error));
+  }
+
+
+  function logout() {
+    var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var requestOptions = {
+          method: 'POST',
+          credentials: 'include',
+          headers: myHeaders,
+          redirect: 'follow'
+      };
+      return fetch("http://freeway.eastus.cloudapp.azure.com:8000/api/logout", requestOptions)
+          .then(response => response.text())
+          .then(result => {
+              this.setState({
+              isLoading: false,
+              dataSource: result,
+              });
+          })
+          .catch(error => console.log('error', error));
+  }
+
+
   const styles = StyleSheet.create({
 
     login:{
